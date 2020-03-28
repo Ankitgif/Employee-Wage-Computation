3cd1bcee60b89a03323cd485fe6ea98340f17087
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 22:44:11 +0530

    [Ankit] Added Solved using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,23 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-isPresent=1
+isFullTime=1
 isPartTime=2
 EMPWAGEPERHR=20
 empCheck=$((RANDOM%3))
-if [ $isPresent -eq $empCheck ]
-then
-	echo "Employee is Present"
-	empWagePerHr=20
-	empHr=8
-	empWage=$(($EMPWAGEPERHR*$empHr))
-	echo "$empWage"
-elif [ $isPartTime -eq $empCheck ]
-then
-	echo "Part Time Employee"
-	empHr=4
-	empWage=$(($EMPWAGEPERHR*empHr))
-	echo "$empWage"
-else
-	echo "Employee is Absent"
-	empWage=0
-	echo "$empWage"
-fi
+case $empCheck in
+	$isFullTime )
+		echo "Full Time Employee"
+		empHr=8
+		;;
+	$isPartTime )
+		echo "Part Time Employee"
+		empHr=4
+		;;
+	* )
+		echo "Employee is Absent";
+		empHr=0
+		echo "$empWage"
+		;;
+esac
+empWage=$(($EMPWAGEPERHR*$empHr))
+echo "$empWage"
