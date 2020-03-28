7842e239d378c0e838e4bb54420dfb252c10b1eb
Ankitgif <ankitsinha2303@gmail.com>
2020-03-29 01:04:35 +0530

    [Ankit] Added function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,33 +2,35 @@
 echo "Welcome to Employee Wage Computation Program"
 IS_FULL_TIME=1
 IS_PART_TIME=2
-MAX_HR_IN_MONTH=100
+MAX_HR_IN_MONTH=4
 EMP_WAGE_PER_HR=20
 NUM_WORKING_DAYS=20
 
 totalEmpHr=0
 totalWorkingDays=0
+function getWorkHr()
+{
+	local empCheck=$1
+	 case $empCheck in
+                $IS_FULL_TIME )
+                        empHr=8
+                        ;;
+                $IS_PART_TIME )
+                        empHr=4
+                        ;;
+                * )
+                        empHr=0
+                        ;;
+        esac
+	echo "$empHr"
+}
 while [[ $totalEmpHr -lt $MAX_HR_IN_MONTH &&
 			$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_FULL_TIME )
-			echo "Full Time Employee"
-			empHr=8
-			;;
-		$IS_PART_TIME )
-			echo "Part Time Employee"
-			empHr=4
-			;;
-		* )
-			echo "Employee is Absent";
-			empHr=0
-			;;
-	esac
-
-	totalEmpHr=$(($totalEmpHr+$empHr))
+	empHr="$( getWorkHr $empCheck )"
+	totalEmpHr=$(( $totalEmpHr + $empHr ))
 done
 totalWage=$(($totalEmpHr*$EMP_WAGE_PER_HR))
 echo "$totalWage"
