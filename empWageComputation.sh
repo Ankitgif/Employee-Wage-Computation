722ef3cef3dd9c344c7a3e38cc356e0f966dd4a5
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 20:36:02 +0530

    [Ankit] Added Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,6 +5,12 @@ empCheck=$((RANDOM%2))
 if [ $isPresent -eq $empCheck ]
 then
 	echo "Employee is Present"
+	empWagePerHr=20
+	empHr=8
+	empWage=$(($empWagePerHr*$empHr))
+	echo "$empWage"
 else
 	echo "Employee is Absent"
+	empWage=0
+	echo "$empWage"
 fi
