dd7127980b095b80c159c2efeaee4d2bdd91ef19
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 20:16:27 +0530

    [Ankit] Added to check employee Attendance

---
+++ empWageComputation.sh
@@ -0,0 +1,10 @@
+#! /bin/bash -x
+echo "Welcome to Employee Wage Computation Program"
+isPresent=1
+empCheck=$((RANDOM%2))
+if [ $isPresent -eq $empCheck ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
