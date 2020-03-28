4b415f058f459b8d5bebfc50824dae571cb4b88c
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 23:09:44 +0530

    [Ankit] Added Calculation of wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,28 @@ echo "Welcome to Employee Wage Computation Program"
 isFullTime=1
 isPartTime=2
 EMPWAGEPERHR=20
-empCheck=$((RANDOM%3))
-case $empCheck in
-	$isFullTime )
-		echo "Full Time Employee"
-		empHr=8
-		;;
-	$isPartTime )
-		echo "Part Time Employee"
-		empHr=4
-		;;
-	* )
-		echo "Employee is Absent";
-		empHr=0
-		echo "$empWage"
-		;;
-esac
-empWage=$(($EMPWAGEPERHR*$empHr))
-echo "$empWage"
+totalWage=0
+numWorkingDays=20
+for(( day=1;day<=numWorkingDays;day++ ))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$isFullTime )
+			echo "Full Time Employee"
+			empHr=8
+			;;
+		$isPartTime )
+			echo "Part Time Employee"
+			empHr=4
+			;;
+		* )
+			echo "Employee is Absent";
+			empHr=0
+			echo "$empWage"
+			;;
+	esac
+	empWage=$(($EMPWAGEPERHR*$empHr))
+	echo "$empWage"
+	totalWage=$(($totalWage+$empWage))
+	echo "$totalWage"
+done
