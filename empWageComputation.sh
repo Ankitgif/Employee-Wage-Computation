3fb80c0cbfa0f79d56ddd60ae30ba4b53ce61882
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 21:09:27 +0530

    [Ankit] Added Part Time Employee and Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,21 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 isPresent=1
-empCheck=$((RANDOM%2))
+isPartTime=2
+EMPWAGEPERHR=20
+empCheck=$((RANDOM%3))
 if [ $isPresent -eq $empCheck ]
 then
 	echo "Employee is Present"
 	empWagePerHr=20
 	empHr=8
-	empWage=$(($empWagePerHr*$empHr))
+	empWage=$(($EMPWAGEPERHR*$empHr))
+	echo "$empWage"
+elif [ $isPartTime -eq $empCheck ]
+then
+	echo "Part Time Employee"
+	empHr=4
+	empWage=$(($EMPWAGEPERHR*empHr))
 	echo "$empWage"
 else
 	echo "Employee is Absent"
