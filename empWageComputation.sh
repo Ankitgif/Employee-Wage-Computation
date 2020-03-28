fea0b75ccc8464da346713745f6e5faa2ab98191
Ankitgif <ankitsinha2303@gmail.com>
2020-03-28 23:49:13 +0530

    [Ankit] Added Calculation of wages till a condition of total working hrs or days is reached for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,30 +1,34 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-isFullTime=1
-isPartTime=2
-EMPWAGEPERHR=20
-totalWage=0
-numWorkingDays=20
-for(( day=1;day<=numWorkingDays;day++ ))
+IS_FULL_TIME=1
+IS_PART_TIME=2
+MAX_HR_IN_MONTH=100
+EMP_WAGE_PER_HR=20
+NUM_WORKING_DAYS=20
+
+totalEmpHr=0
+totalWorkingDays=0
+while [[ $totalEmpHr -lt $MAX_HR_IN_MONTH &&
+			$totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
 	case $empCheck in
-		$isFullTime )
+		$IS_FULL_TIME )
 			echo "Full Time Employee"
 			empHr=8
 			;;
-		$isPartTime )
+		$IS_PART_TIME )
 			echo "Part Time Employee"
 			empHr=4
 			;;
 		* )
 			echo "Employee is Absent";
 			empHr=0
-			echo "$empWage"
 			;;
 	esac
-	empWage=$(($EMPWAGEPERHR*$empHr))
-	echo "$empWage"
-	totalWage=$(($totalWage+$empWage))
-	echo "$totalWage"
+
+	totalEmpHr=$(($totalEmpHr+$empHr))
 done
+totalWage=$(($totalEmpHr*$EMP_WAGE_PER_HR))
+echo "$totalWage"
